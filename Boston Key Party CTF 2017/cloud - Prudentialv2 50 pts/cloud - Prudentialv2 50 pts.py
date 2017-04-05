e0e42ac915af8b1e8472f87aa8ba6d3d59028a13
porpeeranut <porpeeranut@gmail.com>
2017-04-05 14:05:47 +0700

    update BKP2017

--- Boston Key Party CTF 2017/cloud - Prudentialv2 50 pts/cloud - Prudentialv2 50 pts.py
+++ Boston Key Party CTF 2017/cloud - Prudentialv2 50 pts/cloud - Prudentialv2 50 pts.py
@@ -1,18 +1,27 @@
 import requests
 
-# https://shattered.io/
-# https://alf.nu/SHA1
-# https://twitter.com/marcan42/status/835175023425966080/photo/1
-# https://shattered.io/static/shattered.pdf
-# https://shattered.io/static/pdf_format.png
-# https://github.com/cr-marcstevens/sha1collisiondetection
+'''
+https://shattered.io/
+https://alf.nu/SHA1
+https://twitter.com/marcan42/status/835175023425966080/photo/1
+https://shattered.io/static/shattered.pdf
+https://shattered.io/static/pdf_format.png
+https://github.com/cr-marcstevens/sha1collisiondetection
 
-def readfile(filename):
-	f = open(filename).read()
+https://news.ycombinator.com/item?id=13728294
+'The first 320 bytes of the two PDFs released by Google result in the same SHA-1 state'
+'''
+
+def urlencode(data):
 	out = ''
-	for c in f:
+	for c in data:
 		out += '%'+c.encode('hex')
 	return out
 
-r = requests.get('http://54.202.82.13/?name='+readfile('a.pdf')+'&password='+readfile('b.pdf'))
+# solution 1
+r = requests.get('http://54.202.82.13/?name='+urlencode(open('a.pdf').read())+'&password='+urlencode(open('b.pdf').read()))
+print r.text
+
+# solution 2
+r = requests.get('http://54.202.82.13/?name='+urlencode(open('shattered-1.pdf').read()[:320])+'&password='+urlencode(open('shattered-2.pdf').read()[:320]))
 print r.text
